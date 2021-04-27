b40bee645cecf9555d0e6b881d2b6667cd7b0d72
spaceC00kie <kirstie317@gmail.com>
2021-04-26 20:15:36 -0400

    pseudocode complete

---
+++ nth-fib.py
@@ -0,0 +1,10 @@
+# given a number, return its value in the fib sequence
+
+#fib seq - every new number is the sum of the previous two numbers
+
+def fib():
+
+# if n = 1, return 0
+# if n = 2, return 1
+
+# find the previous two numbers (n - 1 and n - 2) and sum results
